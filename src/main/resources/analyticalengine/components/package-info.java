ea73ab8c76b9d8d4fafed543b6199d187485f191
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2014-01-28 11:21:49 -0500

    Moves library functions to package.

---
+++ src/main/resources/analyticalengine/components/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * package-info.java - built-in functions for the Analytical Engine
+ * 
+ * Copyright 2014 Jeffrey Finkelstein.
+ * 
+ * This file is part of analyticalengine.
+ * 
+ * analyticalengine is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * analyticalengine is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * analyticalengine. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Built-in functions for the Analytical Engine.
+ * 
+ * @author Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
+ * @since 0.0.1
+ */
+package analyticalengine.components;
+
