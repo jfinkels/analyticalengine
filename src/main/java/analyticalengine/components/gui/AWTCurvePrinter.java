ba1bf76f49afa490fe6abf07c4719eac3d2a7768
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2014-01-28 16:42:55 -0500

    adds base class for curve printers.

--- src/main/java/analyticalengine/components/gui/AWTCurvePrinter.java
+++ src/main/java/analyticalengine/components/gui/AWTCurvePrinter.java
@@ -1,23 +1,28 @@
+/**
+ * AWTCurvePrinter.java - plots a curve in an AWT window
+ * 
+ * Copyright 2014 Jeffrey Finkelstein.
+ * 
+ * This file is part of analyticalengine.
+ * 
+ * analyticalengine is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * analyticalengine is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * analyticalengine. If not, see <http://www.gnu.org/licenses/>.
+ */
 package analyticalengine.components.gui;
 
-import java.math.BigInteger;
+import analyticalengine.components.AbstractCurvePrinter;
 
-import analyticalengine.components.CurvePrinter;
-
-public class AWTCurvePrinter implements CurvePrinter {
-
-    private BigInteger x = BigInteger.ZERO;
-    private BigInteger y = BigInteger.ZERO;
-    
-    @Override
-    public void setX(BigInteger x) {
-        this.x = x;
-    }
-
-    @Override
-    public void setY(BigInteger y) {
-        this.y = y;
-    }
+public class AWTCurvePrinter extends AbstractCurvePrinter {
 
     @Override
     public void draw() {
@@ -29,12 +34,6 @@ public class AWTCurvePrinter implements CurvePrinter {
     public void move() {
         // TODO Auto-generated method stub
 
-    }
-
-    @Override
-    public void reset() {
-        this.x = BigInteger.ZERO;
-        this.y = BigInteger.ZERO;
     }
 
 }
